597c6d1259dbb84b2656f3af3d44db2e1bea0248
Nagaarjunme21 <121504335+Nagaarjunme21@users.noreply.github.com>
2022-12-27 20:58:50 +0530

    Add files via upload

---
+++ addupdatedelte.java
@@ -0,0 +1,25 @@
+package Arjun;
+
+public class addupdatedelte {
+	
+	static void addmembers(int [] ab, String [] cd) 
+	{
+		
+	}
+	
+	static void updatemember(int [] ab, String [] cd) 
+	{
+		
+	}
+
+	public static void main(String[] args) {
+		int [] memberid ={1,2,3};
+		String [] membername = {"Arjun","Ajith","Vijay"};
+		
+		addmembers(memberid,membername);
+		updatemember(memberid,membername);
+		
+
+	}
+
+}
